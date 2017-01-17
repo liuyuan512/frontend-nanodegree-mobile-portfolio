95da547f0833af601e10a1bf12f3ddefa2f342d4
liuyuan512 <liuyuan512@gmail.com>
2017-01-17 14:47:39 +0800

    Add some image-minifid function

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,36 +1,77 @@
 module.exports = function(grunt) {
+
     var mozjpeg = require('imagemin-mozjpeg');
-    // var jpegtran = require('imagemin-jpegtran');
+    var jpegtran = require('imagemin-jpegtran');
     // Project configuration.
     grunt.initConfig({
+
+
         pkg: grunt.file.readJSON('package.json'),
 
         imagemin: { // Task
             static: { // Target
                 options: { // Target options
-                    optimizationLevel: 3,
+                    optimizationLevel: 7,
                     svgoPlugins: [{
                         removeViewBox: false
                     }],
                     use: [mozjpeg()]
                 },
                 files: { // Dictionary of files
-                    'img/profilepic.min.jpg': 'img/profilepic.jpg' // 'destination': 'source'
-                    // 'img/mobilewebdev.min.jpg': 'img/mobilewebdev.jpg',
-                    // 'img/cam_be_like.min.jpg': 'img/cam_be_like.jpg',
-                    // 'img/2048.min.png':'img/2048.png'
+                    'img/profilepic.min.jpg': 'img/profilepic.jpg', // 'destination': 'source'
+                    'img/mobilewebdev.min.jpg': 'img/mobilewebdev.jpg',
+                    'img/cam_be_like.min.jpg': 'img/cam_be_like.jpg',
+                    'img/2048.min.png':'img/2048.png',
+                    'img/pizzeria.min.jpg':'img/pizzeria.jpg'
                 }
             },
-            dynamic: { // Another target
-                files: [{
-                    expand: true, // Enable dynamic expansion
-                    cwd: 'img/', // Src matches are relative to this path
-                    src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
-                    dest: 'img/' // Destination path prefix
-                }]
-            }
-        },
 
+                  dynamic: { // Another target
+                      files: [{
+                          expand: true, // Enable dynamic expansion
+                          cwd: 'img/', // Src matches are relative to this path
+                          src: ['**/*.{png,jpg,gif}'], // Actual patterns to match
+                          dest: 'img/' // Destination path prefix
+                      }]
+                  }
+              },
+
+  //           imagemin: {
+  //   png: {
+  //     options: {
+  //       optimizationLevel: 5
+  //     },
+  //     files: [
+  //       {
+  //         // Set to true to enable the following options…
+  //         expand: true,
+  //         // cwd is 'current working directory'
+  //         cwd: 'img/',
+  //         src: ['img/*.png'],
+  //         // Could also match cwd line above. i.e. project-directory/img/
+  //         dest: 'img/compressed/',
+  //         ext: '.png'
+  //       }
+  //     ]
+  //   },
+  //   jpg: {
+  //     options: {
+  //       progressive: true
+  //     },
+  //     files: [
+  //       {
+  //         // Set to true to enable the following options…
+  //         expand: true,
+  //         // cwd is 'current working directory'
+  //         cwd: 'img/',
+  //         src: ['img/pizzeria.jpg'],
+  //         // Could also match cwd. i.e. project-directory/img/
+  //         dest: 'img/compressed/',
+  //         ext: '.jpg'
+  //       }
+  //     ]
+  //   }
+  // },
         htmlmin: { // Task
             dist: { // Target
                 options: { // Target options
@@ -69,33 +110,49 @@ module.exports = function(grunt) {
             }
         },
         image: {
+          options: {
+            optimizationLevel: 10
+          },
             static: {
                 options: {
                     pngquant: true,
                     optipng: false,
                     zopflipng: true,
-                    jpegRecompress: false,
+                    jpegRecompress: true,
                     jpegoptim: true,
                     mozjpeg: true,
                     gifsicle: true,
                     svgo: true
                 },
                 files: {
-                    'img/profilepic.min.jpg': 'img/profilepic.jpg', // 'destination': 'source'
-                    'img/mobilewebdev.min.jpg': 'img/mobilewebdev.jpg',
-                    'img/cam_be_like.min.jpg': 'img/cam_be_like.jpg',
-                    'img/2048.min.png': 'img/2048.png'
+                    // 'img/profilepic.min.jpg': 'img/profilepic.jpg', // 'destination': 'source'
+                    // 'img/mobilewebdev.min.jpg': 'img/mobilewebdev.jpg',
+                    // 'img/cam_be_like.min.jpg': 'img/cam_be_like.jpg',
+                    // // 'img/2048.min.png': 'img/2048.png',
+                    // 'img/pizzeria.jpg':'img/pizzeria.jpg'
+                    'views/images/pizzeria.jpg':'views/images/pizzeria.jpg'
                 }
             },
             dynamic: {
                 files: [{
                     expand: true,
-                    cwd: 'img/',
+                    cwd: 'views/images',
                     src: ['**/*.{png,jpg,gif,svg}'],
                     dest: 'img/'
                 }]
             }
+        },
+
+        img: {
+          options: {
+            optimizationLevel: 7
+          },
+        // using only dirs with output path
+        task1: {
+            src: 'img/652245.jpg',
+            dest: 'img/652245.min.jpg'
         }
+      }
 
     });
 
@@ -103,7 +160,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-htmlmin');
 
     // 默认被执行的任务列表。
-    grunt.registerTask('default', ['htmlmin']);
+    grunt.registerTask('default', ['imagemin']);
 
     // 加载包含 "cssmin" 任务的插件。
     grunt.loadNpmTasks('grunt-contrib-cssmin');
@@ -115,5 +172,10 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.loadNpmTasks('grunt-image');
+
+    grunt.loadNpmTasks('grunt-img');
+
+    // grunt.registerTask('imagemin', ['imagemin']);
+    grunt.registerTask('imagejpg', ['imagemin:jpg']);
 
 };
